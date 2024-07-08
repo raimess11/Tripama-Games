0162d6f1f04046f83a153954178892ae0c9a2f5c
Akhtar Muhammad Achsan <64370286+Macrune@users.noreply.github.com>
2024-07-08 13:12:03 +0700

    Story 3 Scene 1 & 2
    Implemented story 3 scene 1 and 2. Havent checked for any bugs but overall should be fine

---
+++ World/Story 3/castle_v_1_s_3_scene_2.gd
@@ -0,0 +1,32 @@
+extends Level
+
+@onready var cut_scene = $CutScene
+
+const Balloon_source = preload("res://NPC/DialogueSystem/Bubble/balloon.tscn")
+@export var dialogue_resource : DialogueResource
+
+func _ready():
+	QuestSystem.connectAllNodes()
+	NpcState.has_interacted.connect(_update_doors)
+	addDoorToDoors()
+	connect_NPC_to_player()
+	player.disable()
+	player.visible = false
+	
+	if data == null:
+		enter_level()
+	
+	nextCutscene("choice2_description")
+	DialogueManager.dialogue_ended.connect(_on_Dialogue_Ended)
+
+func nextCutscene(next_scene):
+	cut_scene.play(next_scene)
+
+func _on_Dialogue_Ended(source):
+	if source == dialogue_resource:
+		nextCutscene("next_description")
+
+func start_intro():
+	var balloon : Node = Balloon_source.instantiate()
+	get_tree().current_scene.add_child(balloon)
+	balloon.start(dialogue_resource, "start")
