f25f3935b58bb274c847ceeaf7b624ddc5bca871
Yohanes Januarico Andiawan <yohan.loadout@gmail.com>
2024-07-19 21:15:01 +0700

    Update CharSelect.gd

--- GUI/CharSelect.gd
+++ GUI/CharSelect.gd
@@ -3,10 +3,10 @@ extends Button
 @export_file var level_path
 
 func _on_pressed():
+	SfxButton.play()
 	if level_path == null:
 		return
 	$".".disabled = true
-	SfxButton.play()
 	Loading.startLoad()
 	await Loading.loading_finished
 	SceneManager.load_new_scene(level_path, "fade_to_black")
